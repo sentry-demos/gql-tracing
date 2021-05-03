b9d651a1dc971c46a838d1e3f9ab10b971a26a43
Dustin Bailey <bailey.dust@gmail.com>
2021-05-03 09:50:00 -0700

    Community inspiration

--- plugin.ts
+++ plugin.ts
@@ -3,6 +3,7 @@ import { ApolloServerPlugin } from "apollo-server-plugin-base";
 import { Context } from "./context";
 
 const plugin: ApolloServerPlugin<Context> = {
+//community inspiration: https://gist.github.com/efueyo/eec3aa5115ea63005e92337f3205ccdb#file-sentry-plugin-ts
   requestDidStart({ request, context }) {
     if (!!request.operationName) {
       context.transaction.setName(request.operationName!);
